60d53c2a4f30809f3a3c18f0f5d621f31c459494
fitzpa <fitz4492@gmail.com>
2022-07-17 23:43:50 -0500

    tenth?

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
-import prisma from '../src/server/db/client';
+// import prisma from '../src/server/db/client';
 // import jsonData from '../public/data/result.json';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const jsonData = [{
   "id": "cl5onycif0001g7w1tnac34pc",
