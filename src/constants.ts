a73d230e110bb8866c69fc8894d57136cc94e754
fitzpa <fitz4492@gmail.com>
2022-07-18 01:38:03 -0500

    small update to getBaseUrl

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000'
 
-export const url = `${baseUrl}/api/trpc`
+function getBaseUrl() {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
+export const url = `${getBaseUrl()}/api/trpc`
